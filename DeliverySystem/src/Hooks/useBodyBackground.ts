4c9d6859a6d16febec9936d6a59c71d1ab9c5ba9
Gianluca Girardi <gl.girardi1@gmail.com>
2024-11-11 19:50:29 -0500

    Fixed backgound rendering

--- DeliverySystem/src/Hooks/useBodyBackground.ts
+++ DeliverySystem/src/Hooks/useBodyBackground.ts
@@ -1,21 +1,38 @@
-import { useEffect } from 'react';
-import {pngPathBuilder} from "../appUtils.ts";
-import {BACKGROUND_COLOR_GRADIENT, NO_REPEAT, BACKGROUND_IMAGE_POSITION_DEFAULT} from "../constants.ts";
-import {EMPTY_STRING} from "../Components/AuthForm/constants.ts";
+import { useEffect } from "react";
+import { pngPathBuilder } from "../appUtils.ts";
+import {
+  BACKGROUND_COLOR_GRADIENT,
+  NO_REPEAT,
+  BACKGROUND_IMAGE_POSITION_DEFAULT,
+} from "../constants.ts";
 
 type UseBodyBackgroundProps = {
-    backgroundImage: string;
-    backgroundPosition?: string;
-    backgroundSize?: string;
+  backgroundImage: string;
+  backgroundPosition?: string;
+  backgroundSize?: string;
 };
 
-export const useBodyBackground = ({ backgroundImage, backgroundPosition = BACKGROUND_IMAGE_POSITION_DEFAULT, backgroundSize = 'cover' }: UseBodyBackgroundProps): void => {
-    useEffect(() => {
-        const originalBackground = document.body.style.background;
-        document.body.style.background = `url(${pngPathBuilder(backgroundImage)}) ${NO_REPEAT} ${backgroundPosition}, ${BACKGROUND_COLOR_GRADIENT}`;
-        return () => {
-            document.body.style.background = originalBackground;
-            document.body.style.backgroundSize = EMPTY_STRING;
-        };
-    }, [backgroundImage, backgroundPosition, backgroundSize]);
+export const useBodyBackground = ({
+  backgroundImage,
+  backgroundPosition = BACKGROUND_IMAGE_POSITION_DEFAULT,
+  backgroundSize = "50%",
+}: UseBodyBackgroundProps): void => {
+  useEffect(() => {
+    const originalBackground = document.body.style.background;
+    const originalBackgroundSize = document.body.style.backgroundSize;
+    const originalBackgroundAttachment =
+      document.body.style.backgroundAttachment;
+
+    document.body.style.background = `url(${pngPathBuilder(
+      backgroundImage
+    )}) ${NO_REPEAT} ${backgroundPosition}, ${BACKGROUND_COLOR_GRADIENT}`;
+    document.body.style.backgroundSize = backgroundSize;
+    document.body.style.backgroundAttachment = "fixed";
+
+    return () => {
+      document.body.style.background = originalBackground;
+      document.body.style.backgroundSize = originalBackgroundSize;
+      document.body.style.backgroundAttachment = originalBackgroundAttachment;
+    };
+  }, [backgroundImage, backgroundPosition, backgroundSize]);
 };
